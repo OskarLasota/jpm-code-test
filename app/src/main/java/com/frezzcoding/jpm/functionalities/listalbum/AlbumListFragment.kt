e5bc71741c6aaf01d1ddd0d12d2e5b9cb118ef39
oskarlasota <frezzyysmite@gmail.com>
2021-05-14 16:19:46 +0100

    setting up item view for each album

--- app/src/main/java/com/frezzcoding/jpm/functionalities/listalbum/AlbumListFragment.kt
+++ app/src/main/java/com/frezzcoding/jpm/functionalities/listalbum/AlbumListFragment.kt
@@ -4,24 +4,37 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.frezzcoding.jpm.R
+import com.frezzcoding.jpm.functionalities.listalbum.adapter.AlbumListAdapter
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_albumlist.*
 
 @AndroidEntryPoint
 class AlbumListFragment : Fragment(R.layout.fragment_albumlist) {
 
     private val viewModel by viewModels<AlbumViewModel>()
+    private lateinit var albumListAdapter : AlbumListAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupAdapter()
         setupObservers()
         viewModel.getAlbums()
     }
 
     private fun setupObservers(){
         viewModel.albums.observe(viewLifecycleOwner, {
-            
+            albumListAdapter.submitList(it)
         })
+    }
+
+    private fun setupAdapter(){
+        albumListAdapter = AlbumListAdapter()
+        recycler_album_list.apply {
+            layoutManager = LinearLayoutManager(requireContext())
+            adapter = albumListAdapter
+        }
     }
 
 }
--- app/src/main/java/com/frezzcoding/jpm/functionalities/listalbum/adapter/AlbumViewHolder.kt
+++ app/src/main/java/com/frezzcoding/jpm/functionalities/listalbum/adapter/AlbumViewHolder.kt
@@ -3,11 +3,12 @@ package com.frezzcoding.jpm.functionalities.listalbum.adapter
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.frezzcoding.jpm.data.models.AlbumDto
+import kotlinx.android.synthetic.main.item_album.view.*
 
 class AlbumViewHolder(view : View) : RecyclerView.ViewHolder(view) {
 
     fun bindTo(album : AlbumDto){
-
+        itemView.tv_album_title.text = album.title
     }
 
 }

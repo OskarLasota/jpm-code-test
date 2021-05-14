af8f62571f2397ddc90a1313e2e816ae5fcb3607
oskarlasota <frezzyysmite@gmail.com>
2021-05-14 15:50:13 +0100

    setting up adapter for displaying the albums

---
+++ app/src/main/java/com/frezzcoding/jpm/functionalities/listalbum/adapter/AlbumListAdapter.kt
@@ -0,0 +1,37 @@
+package com.frezzcoding.jpm.functionalities.listalbum.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import com.frezzcoding.jpm.R
+import com.frezzcoding.jpm.data.models.AlbumDto
+
+class AlbumListAdapter() : ListAdapter<AlbumDto, AlbumViewHolder>(AlbumsDiffUtil()) {
+
+
+    class AlbumsDiffUtil : DiffUtil.ItemCallback<AlbumDto>(){
+        override fun areItemsTheSame(oldItem: AlbumDto, newItem: AlbumDto): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: AlbumDto, newItem: AlbumDto): Boolean {
+            return oldItem.title == newItem.title
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AlbumViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_album, parent, false)
+
+        return AlbumViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+        getItem(position)?.let {
+            holder.bindTo(it)
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/frezzcoding/jpm/functionalities/listalbum/adapter/AlbumViewHolder.kt
@@ -0,0 +1,13 @@
+package com.frezzcoding.jpm.functionalities.listalbum.adapter
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.frezzcoding.jpm.data.models.AlbumDto
+
+class AlbumViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+
+    fun bindTo(album : AlbumDto){
+
+    }
+
+}
